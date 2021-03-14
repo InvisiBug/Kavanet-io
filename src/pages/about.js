95631a60e4dd7dbbaaf27425e33a5c705c5506ea
Matthew Kavanagh <matthew.kavanagh@skybetingandgaming.com>
2021-03-14 15:56:47 +0000

    Tailwind confirmed working

---
+++ src/pages/about.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const AboutPage = () => (
+  <>
+    <h1 className="text-red-900">About the Author</h1>
+    <p>Welcome to my Gatsby site.</p>
+  </>
+);
+export default AboutPage;
