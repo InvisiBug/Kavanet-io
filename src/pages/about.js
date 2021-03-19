577f360c8b8d0f3c7d36532446bbe49084bfa9c4
InvisiBug <matthewjkavanagh@gmail.com>
2021-03-19 20:11:42 +0000

    Push before dots

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,9 +1,14 @@
 import React from "react";
+import { Layout } from "../components/";
 
-const AboutPage = () => (
-  <>
-    <h1 className="text-red-900">About the Author</h1>
-    <p>Welcome to my Gatsby site.</p>
-  </>
-);
+const AboutPage = () => {
+  return (
+    <>
+      <Layout>
+        <h1 className="text-red-900">About me</h1>
+        <p>Welcome to my Gatsby site.</p>
+      </Layout>
+    </>
+  );
+};
 export default AboutPage;
